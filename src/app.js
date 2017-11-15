e70392b76f01a7e2e68ed93594a0b4b7150461bf
IshanJain <ishan@evolphin.com>
2017-11-15 16:47:03 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import fullPage from './libs/javascript.fullPage.min.js';
 import styles from './scss/styles.scss';
+import canvasDots from './js/canvas.js';
 
 fullPage.initialize('#fullpage', {
     anchors: ['Home', 'About', 'Skills', 'Projects', 'ContactMe'],
     css3: true,
     easingcss3: 'ease'
 });
+
+window.onload = function() {
+    canvasDots();
+};
