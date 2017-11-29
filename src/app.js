6603df4698cb76532d0a05c5d719a7f3a825ff4b
Ishan Jain <Ishan@ISHAN-JAIN>
2017-11-29 19:52:04 +0530

    updated projects milestones and contact

--- src/app.js
+++ src/app.js
@@ -3,23 +3,31 @@ import styles from './scss/styles.scss';
 import canvasDots from './js/canvas.js';
 
 fullPage.initialize('#fullpage', {
-    anchors: ['Home', 'About', 'Projects', 'Clients', 'Milestones', 'ContactMe'],
+    anchors: ['Home', 'About', 'Projects', 'Clients', 'ContactMe', 'Milestones'],
     css3: true,
     easingcss3: 'ease'
 });
 
-window.openModal = function() {
-    const modal = document.getElementById('modal-container');    
-    modal.removeAttribute("class");
-    modal.classList.add('one');
-    document.body.classList.add('modal-active');
-}
+// window.openModal = function() {
+//     const modal = document.getElementById('modal-container');    
+//     modal.removeAttribute("class");
+//     modal.classList.add('one');
+//     document.body.classList.add('modal-active');
+// }
 
-window.closeModal = function() {
-    const modal = document.getElementById('modal-container');
-    modal.classList.add('out');
-    document.body.classList.remove('modal-active');
-}
+// window.closeModal = function() {
+//     const modal = document.getElementById('modal-container');
+//     modal.classList.add('out');
+//     document.body.classList.remove('modal-active');
+// }
+
+// window.addEventListener("hashchange", onHashUpdate, false);
+
+// function onHashUpdate(e) {
+//     const hashLocation = e.target.location.hash.replace('#', '').toLowerCase();
+//     const domeNode = document.getElementById(hashLocation);
+//     domeNode.style.visibility = "visible";
+// }
 
 window.onload = function() {
     canvasDots();
